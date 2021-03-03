a4eef041be5d7e38356c37ebdd81bba0ebd2485e
Filomaster <fpm1504@gmail.com>
2021-03-03 23:53:30 +0100

    Added viruses dance, works fine expect NES

--- scripts/data.js
+++ scripts/data.js
@@ -3,11 +3,12 @@
 // All data and structures, shared in other scripts; equivalent of enumerators
 let Data = {
   // All possible field status, empty, 3x color and 3x viruses
-  Field: { empty: 0, red: 1, yellow: 2, blue: 3, virus_r: 11, virus_y: 12, virus_b: 13 },
+  Field: { empty: 0, red: 1, blue: 2, yellow: 3, virus_r: 11, virus_b: 12, virus_y: 13 },
   State: { movement: 0, clear: 1, gravity: 2, shifting: 3, win: 4, lose: 5, menu: 6, animation: 7 }, // Game state, for performance optimization in the main loop
+  VirusState: { dancing: 0, knocked: 1, laughing: 2 },
   PlayerMode: { single: 1, multi: 2 }, // Possible game modes, based on original game
   EmulationMode: { ATARI: 0, NES: 1, GB: 2, GBC: 3 }, // Emulation mode. Some versions may have slightly different mechanics
-  Colors: ["rd", "yl", "bl"],
+  Colors: ["rd", "bl", "yl"],
   ScreenSize: { ATARI: { w: 40, h: 24 }, NES: { w: 32, h: 30 }, GB: { w: 20, h: 18 } },
   ColorsNES: { bcg: ["#005200", "#3900A5", "#808080"] },
   ColorsATARI: { bcg: ["#621C73", "#008267", "#ffb68c", "#837e85", "#123eb2"] },
@@ -33,6 +34,20 @@ let Data = {
     {
       l: [-9, -20, 0, -9, -10, 9], //-34, -12, -1, -1, -1, -1, 0, -1, -1, 11, 12, 12, 12],
       r: [-19, -10, -10, -1, 0, -1], //-34, 0, -1, 12, -1, -1, -13, -1, 12, 11, 12, 12, 12],
+    },
+  ],
+  VirusFrames: [
+    {
+      x: [2, 2, 3, 3, 4, 5, 6, 7, 8, 9, 9, 8, 8, 7, 6, 5, 4, 3, 2], //3, 4, 4, 5, 6, 7, 8, 9, 9, 9, 9, 8, 6, 3, 2], //3, 6, 8],
+      y: [5, 4, 3, 2, 2, 2, 2, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 6, 5], //4, 3, 2, 2, 2, 2, 2, 3, 4, 5, 6, 7, 7, 6, 5], //4, 7, 2],
+    },
+    {
+      x: [3],
+      y: [7],
+    },
+    {
+      x: [12.5, 14.5, 16.5],
+      y: [0.5, 0.5, 0.5],
     },
   ],
   MaximumRow: [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 11, 11, 12],
